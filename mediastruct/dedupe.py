0804e3a55c032530d6faf6a2e7840a458ea5d461
Charlie Root <root@bsd1.perpetualmedia.com>
2018-12-24 10:19:21 -0800

    latest

--- mediastruct/dedupe.py
+++ mediastruct/dedupe.py
@@ -6,7 +6,7 @@ import glob
 import re
 import itertools
 import shutil
-
+from collections import Counter
 log = logging.getLogger(__name__)
 log.info('Launching the Dedupe Class')
 
@@ -37,36 +37,52 @@ class dedupe:
     #cycle through the dataset and find duplicate entries
     def dups(self, array, duplicates_dir):
         #init dictionaries
-        key_values = {}
+        dictlist = {}
         to_delete = {}
+        id_list = []
+        dictlist_line = {}
         #loop thru combined dataset
         arraylen = len(array)
         for d in array:
-            arraylen = arraylen - 1
-            print("Remaining Records: ", arraylen)
-            #loop through each item in the combined dataset, searching for identical hashes
-            for e in array:
-                #init dictionary
-                index_line = {}
-                match = 0
-                #omit the du record in the json
-                if d != 'du' and e != 'du':
-                    #as we loop through match hashes that don't have identical path/name
-                    if array[d]['filehash'] == array[e]['filehash'] and array[d]['path'] != array[e]['path']:
-                        log.info("Duplicate: %s - %s - %s -  %s" % ( array[e]['filehash'],array[d]['path'],array[e]['path'], array[e]['filesize']))
-                        index_line.update([ ('filehash',array[e]['filehash']) , ('path',array[e]['path']) , ('filesize',array[e]['filesize']) ])
-                        #the vars say delete, but we're not deleting anything don't worry
-                        #this is the only way my brain could figure out how to take only the first matching record, open to better ideas 
-                        for deletes in to_delete:
-                            if array[e]['filehash'] == to_delete[deletes]['filehash'] or e == deletes:
-                                match = 1
-                        #make sure our file to be "deleted" (not deleted) is not in the archive already
-                        if match != 1 and 'archive' not in array[e]['path']: 
-                            to_delete[e] = index_line
-        #loop through the "to be deleted" files and move them to the duplicates directory
-        for key in to_delete:
-            if os.path.isfile(array[key]['path']):
-                log.info("Moving Duplicate %s to %s" % (array[key]['path'],duplicates_dir))
-                shutil.move(array[key]['path'],duplicates_dir + '/')
-            del array[key]
+            dictlist_line = []
+            if d != 'du':
+                #dictlist_line = ([ ('id',d) ])
+                #print(d)
+                dictlist_line = ([ ('id',d) ])
+                dictlist[array[d]['filehash']] = dictlist_line
+            results = [k for k,v in Counter(dictlist[d]) if len(v)>1]
+        print(results)
+        for i in range(len(results)):
+            print("%s %s" % (results[i]['id'],results[i]))
+#            for deletes in to_delete:
+#                if to_delete[deletes]['id'] not in id_list:
+#                    id_list.append(to_delete[deletes]['id'])
+#            arraylen = arraylen - 1
+#            print("Remaining Records: ", arraylen)
+#            #loop through each item in the combined dataset, searching for identical hashes
+#            if d not in id_list:
+#                for e in array:
+#                    #init dictionary
+#                    index_line = {}
+#                    match = 0
+#                    #omit the du record in the json
+#                    if d != 'du' and e != 'du' and 'archive' not in array[e]['path']:
+#                        #as we loop through match hashes that don't have identical path/name
+#                        if array[d]['filehash'] == array[e]['filehash'] and array[d]['path'] != array[e]['path']:
+#                            index_line.update([ ('id',e) , ('filehash',array[e]['filehash']) , ('path',array[e]['path']) , ('filesize',array[e]['filesize']) ])
+#                            #for deletes in to_delete:
+#                            #    if array[e]['filehash'] == to_delete[deletes]['filehash'] or e == deletes:
+#                            #        match = 1
+#                            #if match != 1 and 'archive' not in array[e]['path']: 
+#                            #print(e)
+#                            if e not in to_delete.values():
+#                                log.info("Duplicate: %s - %s - %s -  %s" % ( array[e]['filehash'],array[d]['path'],array[e]['path'], array[e]['filesize']))
+#                                to_delete[e] = index_line
+#                                #print(to_delete)
+#        #loop through the "to be deleted" files and move them to the duplicates directory
+#        for key in to_delete:
+#            if os.path.isfile(array[key]['path']):
+#                log.info("Moving Duplicate %s to %s" % (array[key]['path'],duplicates_dir))
+#                shutil.move(array[key]['path'],duplicates_dir + '/')
+#            del array[key]
 
