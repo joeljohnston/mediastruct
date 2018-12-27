c9cc1400b4838a5fb1dfceff5587ff354a27e816
joeljohnston <joel@perpetual.media>
2018-12-27 13:30:54 -0800

    latest with dupes not working

--- mediastruct/dedupe.py
+++ mediastruct/dedupe.py
@@ -6,6 +6,9 @@ import glob
 import re
 import itertools
 import shutil
+import itertools
+from itertools import chain
+from collections import OrderedDict
 from collections import Counter
 log = logging.getLogger(__name__)
 log.info('Launching the Dedupe Class')
@@ -37,52 +40,48 @@ class dedupe:
     #cycle through the dataset and find duplicate entries
     def dups(self, array, duplicates_dir):
         #init dictionaries
-        dictlist = {}
+        dictlist = [] 
+        dictordered = OrderedDict()
+        duplicate = []
+        duplicates = []
         to_delete = {}
         id_list = []
         dictlist_line = {}
         #loop thru combined dataset
         arraylen = len(array)
+
         for d in array:
-            dictlist_line = []
             if d != 'du':
                 #dictlist_line = ([ ('id',d) ])
                 #print(d)
-                dictlist_line = ([ ('id',d) ])
-                dictlist[array[d]['filehash']] = dictlist_line
-            results = [k for k,v in Counter(dictlist[d]) if len(v)>1]
-        print(results)
-        for i in range(len(results)):
-            print("%s %s" % (results[i]['id'],results[i]))
-#            for deletes in to_delete:
-#                if to_delete[deletes]['id'] not in id_list:
-#                    id_list.append(to_delete[deletes]['id'])
-#            arraylen = arraylen - 1
-#            print("Remaining Records: ", arraylen)
-#            #loop through each item in the combined dataset, searching for identical hashes
-#            if d not in id_list:
-#                for e in array:
-#                    #init dictionary
-#                    index_line = {}
-#                    match = 0
-#                    #omit the du record in the json
-#                    if d != 'du' and e != 'du' and 'archive' not in array[e]['path']:
-#                        #as we loop through match hashes that don't have identical path/name
-#                        if array[d]['filehash'] == array[e]['filehash'] and array[d]['path'] != array[e]['path']:
-#                            index_line.update([ ('id',e) , ('filehash',array[e]['filehash']) , ('path',array[e]['path']) , ('filesize',array[e]['filesize']) ])
-#                            #for deletes in to_delete:
-#                            #    if array[e]['filehash'] == to_delete[deletes]['filehash'] or e == deletes:
-#                            #        match = 1
-#                            #if match != 1 and 'archive' not in array[e]['path']: 
-#                            #print(e)
-#                            if e not in to_delete.values():
-#                                log.info("Duplicate: %s - %s - %s -  %s" % ( array[e]['filehash'],array[d]['path'],array[e]['path'], array[e]['filesize']))
-#                                to_delete[e] = index_line
-#                                #print(to_delete)
-#        #loop through the "to be deleted" files and move them to the duplicates directory
-#        for key in to_delete:
-#            if os.path.isfile(array[key]['path']):
-#                log.info("Moving Duplicate %s to %s" % (array[key]['path'],duplicates_dir))
-#                shutil.move(array[key]['path'],duplicates_dir + '/')
-#            del array[key]
+                dictlist_line = (d,array[d]['filehash'])
+                dictlist.append(dictlist_line)
+                #dictlist[array[d]['filehash']] = dictlist_line
+                #results = [k for k,v in Counter(dictlist[d]['id']) if len(v)>1]
+                #results = [k for k in Counter(dictlist[d]['id']) if len(k)>1]
+        
+        for i,j in dictlist:
+            #print('%s,%s' % (i,j))
+            dictordered.setdefault(j,[]).append(i)
 
+        to_delete = list(chain.from_iterable([j for i, j in dictordered.items() if len(j)>1]))
+
+        #loop through the "to be deleted" files and move them to the duplicates directory
+        for key in to_delete:
+            if os.path.isfile(array[key]['path']):
+                log.info("Moving Duplicate %s to %s" % (array[key]['path'],duplicates_dir))
+                #get year
+                fullpath = re.split(r"\/",array[key][0]['path'])
+                year = fullpath[3]
+                dest_dir = archive_dir + '/' + str(files_to_archive[h][0]['volume']) + '/' + year + '/' + fullpath[4]
+                dest_path = archive_dir + '/' + str(files_to_archive[h][0]['volume']) + '/' + year + '/' + fullpath[4] + '/' + fullpath[5]
+                if not os.path.isdir(dest_dir):
+                    utils.mkdir_p(self, dest_dir)
+                from_path =  files_to_archive[h][0]['path']
+                log.info("Archiving %s to %s" % (from_path, dest_path))
+                if os.path.isfile(files_to_archive[h][0]['path']):
+                    print("Moving :", files_to_archive[h][0]['path'])
+                    shutil.move(files_to_archive[h][0]['path'], dest_path)
+                
+                shutil.move(array[key]['path'],duplicates_dir + '/')
+            #del array[key]
